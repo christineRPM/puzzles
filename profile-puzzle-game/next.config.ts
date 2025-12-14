647f9d90e7be04b90dc908f2c2eac070a0b9d902
Christine <christine@embervest.com>
2025-12-13 16:27:25 -1000

    add vercel file

--- profile-puzzle-game/next.config.ts
+++ profile-puzzle-game/next.config.ts
@@ -9,6 +9,37 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    // Ignore optional dependencies that aren't needed in browser environment
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        '@react-native-async-storage/async-storage': false,
+        'pino-pretty': false,
+      };
+      
+      // Add alias to ignore these modules
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@react-native-async-storage/async-storage': false,
+        'pino-pretty': false,
+      };
+    }
+    
+    // Ignore these modules during webpack bundling
+    config.plugins = config.plugins || [];
+    const { IgnorePlugin } = require('webpack');
+    config.plugins.push(
+      new IgnorePlugin({
+        resourceRegExp: /^@react-native-async-storage\/async-storage$/,
+      }),
+      new IgnorePlugin({
+        resourceRegExp: /^pino-pretty$/,
+      })
+    );
+    
+    return config;
+  },
 };
 
 export default nextConfig;
